985dafdc099fbaf17ee069554b3a863af2e0f635
isaki0317 <matsuoisaki.0317@gmail.com>
2021-01-22 06:42:39 +0000

    [update]レストラン一覧完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  before_action :fake_load
+
+  # material-ui(ローディング時の挙動)の実行確認用
+  def fake_load
+    sleep(1)
+  end
+
 end
--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,9 +1,21 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
 import { fetchRestaurants } from '../apis/restaurants';
+import { Link } from "react-router-dom";
+// components
+import Skeleton from '@material-ui/lab/Skeleton';
+// constants
+import { REQUEST_STATE } from '../constants';
 //images
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
+import RestaurantImage from '../images/restaurant-image.jpg';
+//Reducers
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
 
 //変数にHTMLタグとstyleをセットで格納するイメージ
 const HeaderWrapper = styled.div`
@@ -20,13 +32,42 @@ const MainCoverImageWrapper = styled.div`
 const MainCover = styled.img`
   height: 600px;
 `;
+const RestaurantsContentsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 150px;
+`;
+const RestaurantsContentWrapper = styled.div`
+  width: 410px;
+  height: 300px;
+  padding: 48px;
+`;
+const RestaurantsImageNode = styled.img`
+  width: 90%;
+`;
+const MainText = styled.p`
+  color: black;
+  font-size: 18px;
+`;
+const SubText = styled.p`
+  color: black;
+  font-size: 12px;
+`;
 
 export const Restaurants = () => {
 
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
   useEffect(() => {
+    dispatch({ type: restaurantsActionTyps.FETCHING });
     fetchRestaurants()
     .then((data) =>
-    console.log(data)
+      dispatch({
+        type: restaurantsActionTyps.FETCH_SUCCESS,
+        payload: {
+          restaurants: data.restaurants
+        }
+      })
     )
   }, [])
 
@@ -38,6 +79,26 @@ export const Restaurants = () => {
       <MainCoverImageWrapper>
         <MainCover src={MainCoverImage} alt="main cover" />
       </MainCoverImageWrapper>
+      <RestaurantsContentsList>
+        {
+          state.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+              <Skeleton variant="rect" width={450} height={300} />
+            </Fragment>
+          :
+            state.restaurantsList.map((item, index) =>
+              <Link to={`/restaurants/${item.id}/foods`} key={index} style={{ textDecoration: 'none' }}>
+                <RestaurantsContentWrapper>
+                  <RestaurantsImageNode src={RestaurantImage} />
+                  <MainText>{item.name}</MainText>
+                  <SubText>{`配送料：${item.fee}円 ${item.time_required}分`}</SubText>
+                </RestaurantsContentWrapper>
+              </Link>
+            )
+        }
+      </RestaurantsContentsList>
     </Fragment>
   )
 }
